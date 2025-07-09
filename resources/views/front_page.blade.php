8e7b8cd825e0351d10dd7322fd718329ef96508a
MuhammadMuheet <harddetermination@gmail.com>
2025-07-09 21:03:40 +0500

    Country code update

--- resources/views/front_page.blade.php
+++ resources/views/front_page.blade.php
@@ -1369,13 +1369,14 @@
             <h2 class="section-title">Global Reach, UAE Focused</h2>
             <p class="section-subtitle">We primarily serve the UAE market with shipping connections to neighboring
                 countries for expanded opportunities.</p>
+
             <div class="country-hub">
                 <div class="country-orbit">
-                    <img class="country-point" src="countries/Bangladesh.png" alt="Bangladesh flag" style="object-fit: cover; object-position: center;">
-                    <img class="country-point" src="countries/Sri-lanka.png"  alt="Sri Lanka flag" style="object-fit: cover; object-position: center;">
+                    <img class="country-point" src="countries/bangladesh.png" alt="Bangladesh flag" style="object-fit: cover; object-position: center;">
+                    <img class="country-point" src="countries/sri-lanka.png"  alt="Sri Lanka flag" style="object-fit: cover; object-position: center;">
                     <img class="country-point" src="countries/pakistan.png"  alt="Pakistan flag" style="object-fit: cover; object-position: center;">
                     <img class="country-point" src="countries/india.png"  alt="India flag" style="object-fit: cover; object-position: center;">
-                    <img class="country-point" src="countries/Afghanistan.png" alt="Afghanistan flag" style="object-fit: cover; object-position: center;">
+                    <img class="country-point" src="countries/afghanistan.png" alt="Afghanistan flag" style="object-fit: cover; object-position: center;">
                     <img class="country-point" src="countries/iran.png" alt="Iran flag" style="object-fit: cover; object-position:center;">
                 </div>
                 <div class="uae-center">
